c578f854cdb93acbbf742c4aa290422feda43f45
Andrea Righi <andrea.righi@canonical.com>
2020-11-27 18:40:01 +0000

    [PATCH 4804/4807] Linux 5.10 compat: frame.h renamed objtool.h
    Gbp-Pq: 4804-Linux-5.10-compat-frame.h-renamed-objtool.h.patch.

--- include/sys/frame.h
+++ include/sys/frame.h
@@ -24,7 +24,11 @@ extern "C" {
 #endif
 
 #if defined(__KERNEL__) && defined(HAVE_STACK_FRAME_NON_STANDARD)
+#if defined(HAVE_KERNEL_OBJTOOL_HEADER)
+#include <linux/objtool.h>
+#else
 #include <linux/frame.h>
+#endif
 #else
 #define	STACK_FRAME_NON_STANDARD(func)
 #endif
